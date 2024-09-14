44c033695d419922fc6daa033418a46bcc6f4a42
jp-999 <jayanthpeddi999@gmail.com>
2024-09-14 14:32:50 +0530

    adding images and modifing app.js location and adding fonts

---
+++ my-app/screens/Profile.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <text>Profile Page</text>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
